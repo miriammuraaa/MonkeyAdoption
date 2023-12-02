b70c97d83e6d9b0c30437551c741c34a9570c449
Miriam Murà <muracalamardo@gmail.com>
2023-12-02 16:54:42 +0100

    bu

--- index.php
+++ index.php
@@ -3,26 +3,28 @@ session_start();
 
 require 'database.php';
 
-if (isset($_SESSION['id'])) {
-    $records = $conn->prepare('SELECT id, email, password FROM users WHERE id = :id');
-    $records->bindParam(':id', $_SESSION['id']);
+if (!empty($_POST['email']) && !empty($_POST['password'])) {
+    $records = $conn->prepare('SELECT id, email, password FROM users WHERE email = :email');
+    $records->bindParam(':email', $_POST['email']);
     $records->execute();
     $results = $records->fetch(PDO::FETCH_ASSOC);
 
-    $user = null;
+    $message = '';
 
-    if (count($results) > 0) {
-        $user = $results;
+    if (count($results) > 0 && password_verify($_POST['password'], $results['password'])) {
+        $_SESSION['id'] = $results['id'];
+        header("Location: /MonkeyAdoption");
+    } else {
+        $message = 'Sorry, those credentials do not match';
     }
 }
 ?>
 
 <!DOCTYPE html>
 <html>
-
 <head>
     <meta charset="utf-8">
-    <title>Welcome to you WebApp</title>
+    <title>Login</title>
     <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
     <link rel="stylesheet" href="assets/css/style.css">
 </head>
@@ -39,10 +41,34 @@ if (isset($_SESSION['id'])) {
         </a>
         
     <?php else: ?>
-        <h1>Please Login or SignUp</h1>
+        <?php if (!empty($message)): ?>
+        <p>
+            <?= $message ?>
+        </p>
+    <?php endif; ?>
 
-        <a href="login.php">Login</a> or
-        <a href="signup.php">SignUp</a>
+    <div class="caja_global">
+        <div class="caja_principal_log">
+            <div>
+                <h1>Create Account</h1>
+                <form action="login.php" method="POST">
+                    <input name="email" type="text" placeholder="Enter your email">
+                    <input name="password" type="password" placeholder="Enter your Password">
+                    <input type="submit" id="sign_in_log" value="SIGN IN">
+                </form>
+            </div>
+        </div>
+
+        <div class="caja_secundaria_log">
+            <div>
+                <h1>Hello, Friend!</h1>
+                <p>Register whith your personal details to use all of site features</p>
+                <a href="signup.php">
+                    <input type="sign_up_log" value="SIGN UP">
+                </a>
+            </div>
+        </div>
+    </div>
     <?php endif; ?>
 </body>
 
